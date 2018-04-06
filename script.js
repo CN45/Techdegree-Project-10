803ef70ad5679314b71d687dda7b46806359a25c
Cyndi Norwood <cyndinorwood@me.com>
2018-04-06 12:56:05 -0700

    Add border around pics

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var randomuserAPI = 'https://randomuser.me/api/?results=12';
   function displayPhotos(data) {
        var photoHTML = '<ul>';
        $.each( data.results, function (i, photo) {
+
          photoHTML += '<li class="pic">';
          photoHTML += '<a href="' + photo.picture.large + ' " class="image">';
          photoHTML += '<img src="' + photo.picture.large + '"></a></li>';
@@ -14,8 +15,10 @@ var randomuserAPI = 'https://randomuser.me/api/?results=12';
          photoHTML += '<p class="caps">' + photo.name.first  + " "+ photo.name.last + '</li>';
          photoHTML += '<p>' + photo.email + '</li>';
          photoHTML += '<p class="location">' + photo.location.city + '</li>';
+
        });
         photoHTML += '</ul>';
+
        $('#photos').append(photoHTML);
        $('#photos').append(photoHTML);
 

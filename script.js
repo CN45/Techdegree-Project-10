49c87de793b63d5fd4ea0b3c9a421e91a1a15efd
Cyndi Norwood <cyndinorwood@me.com>
2018-04-07 12:41:13 -0700

    Mobile alignment
    Fixed mobile view alignment of pics

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-
-$(document).ready(function () {
+$(document).ready(function() {
   $(function() {
-var randomuserAPI = 'https://randomuser.me/api/?results=12';
+    var randomuserAPI = 'https://randomuser.me/api/?results=12';
 
 
-  function displayPhotos(data) {
-       var photoHTML = '<ul class="line">';
+    function displayPhotos(data) {
+      var photoHTML = '<ul class="line">';
 
-       $.each( data.results, function (i, photo) {
-       photoHTML += '<div class="surround">';
-         photoHTML += '<li class="line">';
-         photoHTML += '<a href="' + photo.picture.large + ' " class="image">';
-         photoHTML += '<img src="' + photo.picture.large + '"></a></li>';
-         photoHTML += '<li class="pic">';
-         photoHTML += '<p class="caps">' + photo.name.first  + " "+ photo.name.last + '</li>';
-         photoHTML += '<p>' + photo.email + '</li>';
-         photoHTML += '<p class="location">' + photo.location.city + '</li>';
-photoHTML += '</div>';
-       });
+      $.each(data.results, function(i, photo) {
+        photoHTML += '<div class="surround">';
+        photoHTML += '<li class="line">';
+        photoHTML += '<a href="' + photo.picture.large + ' " class="image">';
+        photoHTML += '<img src="' + photo.picture.large + '"></a></li>';
+        photoHTML += '<li class="pic">';
+        photoHTML += '<p class="caps">' + photo.name.first + " " + photo.name.last + '</li>';
+        photoHTML += '<li class="pic">';
+        photoHTML += '<p>' + photo.email + '</li>';
+        photoHTML += '<li class="pic">';
+        photoHTML += '<p class="location">' + photo.location.city + '</li>';
+        photoHTML += '</div>';
+      });
 
 
-    photoHTML += '</ul>';
-       $('#photos').append(photoHTML);
-       $('#photos').append(photoHTML);
+      photoHTML += '</ul>';
+      $('#photos').append(photoHTML);
+      $('#photos').append(photoHTML);
 
-     }
+    }
 
-     $.getJSON(randomuserAPI, displayPhotos);
-   });
- });
+    $.getJSON(randomuserAPI, displayPhotos);
+  });
+});

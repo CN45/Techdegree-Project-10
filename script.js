52c9e5815c6b3d74bc889ba73ac9cdf331ef7cc3
Cyndi Norwood <cyndinorwood@me.com>
2018-04-18 11:54:06 -0700

    modal fixes

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 $(document).ready(function() {
 
-let members;
-let selectedMemberIndex = 0;
-let modalContainer = document.querySelector('.modal-content');
-let key = 0;
-const span = document.getElementById("#close");
+  let members;
+  let selectedMemberIndex = 0;
+  let modalContainer = document.querySelector('.modal-content');
+  let key = 0;
+  const span = document.getElementById("#close");
 
-function displayModalForUser(member){
-let modalHTML = "";
+  function displayModalForUser(member) {
+    let modalHTML = "";
 
-  modalHTML += '<div class="modalDiv">';
-  modalHTML += '<li class="modalLine">';
-  modalHTML += '<img class="picModal" src="' + member.picture.large + '"></li>';
-  modalHTML += '<li class="picModal">';
-  modalHTML += '<p class="center">' + member.name.first + " " + member.name.last + '</li>';
-  modalHTML += '<li class="picModal">';
-  modalHTML += '<p>' + member.email + '</li>';
-  modalHTML += '<p class="center">' + member.location.city + '</li>';
-  modalHTML += '<p class="center">' + member.phone + '</li>';
-  modalHTML += '<p class="inline">' + member.location.street + '</li>';
-  modalHTML += '<p class="inline">' + member.location.city + " " + '</li>';
-  modalHTML += '<p class="inline">' + member.location.state + " " + '</li>';
-  modalHTML += '<p class="inline">' + member.location.postcode + '</li>';
-  modalHTML += '<p class="center">' + '<p>Birthday</p>' + new Date(member.dob).toLocaleDateString() + '</li>';
-  modalHTML += '</div>';
-  modalContainer.innerHTML += modalHTML;
-}
-$('.modal').hide();//hide the modal on page load
+    modalHTML += '<div class="modalDiv">';
+    modalHTML += '<li class="modalLine">';
+    modalHTML += '<img class="picModal" src="' + member.picture.large + '"></li>';
+    modalHTML += '<li class="picModal">';
+    modalHTML += '<p class="center">' + member.name.first + " " + member.name.last + '</li>';
+    modalHTML += '<li class="picModal">';
+    modalHTML += '<p>' + member.email + '</li>';
+    modalHTML += '<p class="center">' + member.location.city + '</li>';
+    modalHTML += '<hr></hr>';
+    modalHTML += '<p class="phone">' + member.phone + '</li>';
+    modalHTML += '<p class="inline">' + member.location.street + '</li>';
+    modalHTML += '<p class="inline">' + member.location.city + ", " + '</li>';
+    modalHTML += '<p class="inline">' + member.location.state + " " + '</li>';
+    modalHTML += '<p class="inline">' + member.location.postcode + '</li>';
+    modalHTML += '<p class="center">' + '<p>Birthday</p>' + new Date(member.dob).toLocaleDateString() + '</li>';
+    modalHTML += '</div>';
+    modalContainer.innerHTML += modalHTML;
+  }
+  $('.modal').hide(); //hide the modal on page load
 
 
   $(function() {
@@ -50,7 +51,7 @@ $('.modal').hide();//hide the modal on page load
         photoHTML += '<li class="pic">';
         photoHTML += '<p class="location">' + photo.location.city + '</li>';
         photoHTML += '</div>';
-        key ++;
+        key++;
       });
 
       photoHTML += '</ul>';
@@ -61,61 +62,61 @@ $('.modal').hide();//hide the modal on page load
         displayModalForUser(members[memberIndex]);
         $('.modal').show();
         $('#close').show();
-        $("#close").on("click", function(e){
-            $('.modalDiv').remove();
-            $('.modal').hide();
+        $("#close").on("click", function(e) {
+          $('.modalDiv').remove();
+          $('.modal').hide();
 
         });
 
       });
 
-}
+    }
 
     $.getJSON(randomuserAPI, displayPhotos);
-/*    $(function() {
-      var modalAPI = 'https://randomuser.me/api/';
+    /*    $(function() {
+          var modalAPI = 'https://randomuser.me/api/';
 
 
-      function displayPhotos(data) {
-        var modalHTML = '<ul class="modal-content">';
+          function displayPhotos(data) {
+            var modalHTML = '<ul class="modal-content">';
 
-        $.each(data.results, function(i, photo) {
+            $.each(data.results, function(i, photo) {
 
-          modalHTML += '<div class"mod">';
-          modalHTML += '<li class="modalLine">';
-          modalHTML += '<a href="' + photo.picture.large + ' " class="picModal">';
-          modalHTML += '<img class="picModal" src="' + photo.picture.large + '"></a></li>';
-          modalHTML += '<li class="picModal">';
+              modalHTML += '<div class"mod">';
+              modalHTML += '<li class="modalLine">';
+              modalHTML += '<a href="' + photo.picture.large + ' " class="picModal">';
+              modalHTML += '<img class="picModal" src="' + photo.picture.large + '"></a></li>';
+              modalHTML += '<li class="picModal">';
 
-          modalHTML += '<p class="center">' + photo.name.first + " " + photo.name.last + '</li>';
-          modalHTML += '<li class="picModal">';
-          modalHTML += '<p>' + photo.email + '</li>';
-          modalHTML += '<p class="center">' + photo.location.city + '</li>';
-          modalHTML += '<p class="center">' + photo.phone + '</li>';
-          modalHTML += '<p class="center">' + photo.location.street + '</li>';
-          modalHTML += '<p class="center">' + photo.location.city + '</li>';
-          modalHTML += '<p class="center">' + photo.location.state + '</li>';
-          modalHTML += '<p class="center">' + photo.location.postcode + '</li>';
+              modalHTML += '<p class="center">' + photo.name.first + " " + photo.name.last + '</li>';
+              modalHTML += '<li class="picModal">';
+              modalHTML += '<p>' + photo.email + '</li>';
+              modalHTML += '<p class="center">' + photo.location.city + '</li>';
+              modalHTML += '<p class="center">' + photo.phone + '</li>';
+              modalHTML += '<p class="center">' + photo.location.street + '</li>';
+              modalHTML += '<p class="center">' + photo.location.city + '</li>';
+              modalHTML += '<p class="center">' + photo.location.state + '</li>';
+              modalHTML += '<p class="center">' + photo.location.postcode + '</li>';
 
-          modalHTML += '<p class="center">' + '<p>Birthday</p>' + photo.dob + '</li>';
-
-
+              modalHTML += '<p class="center">' + '<p>Birthday</p>' + photo.dob + '</li>';
 
 
-          modalHTML += '</div>';
 
+
+              modalHTML += '</div>';
+
+            });
+
+
+            modalHTML += '</ul>';
+            $('.modal-content').append(modalHTML);
+
+
+          }
+
+          $.getJSON(modalAPI, displayPhotos);
         });
-
-
-        modalHTML += '</ul>';
-        $('.modal-content').append(modalHTML);
-
-
-      }
-
-      $.getJSON(modalAPI, displayPhotos);
-    });
-*/
+    */
 
 
   });
